eb524d507c1345238b32d6a7140fa7d6e9bfc77c
jeff <ampernand@gmail.com>
2015-05-08 09:25:16 -0400

    frontend change db schema to use mysql compatable columns

--- nntpchan/database.py
+++ nntpchan/database.py
@@ -8,16 +8,14 @@ database primatives
 """
 
 from sqlalchemy import *
-from sqlalchemy.dialects.postgres import UUID as GUID
 
 from . import config
 
 _meta = MetaData()
 
 posts = Table('frontend_posts', _meta, 
-              Column('article_id', Text, unique=True),
+              Column('article_id', Text, primary_key=True),
               Column('article_id_hash', Text),
-              Column('id', GUID, primary_key=True),
               Column('newsgroup', Text, nullable=False),
               Column('parent', Text),
               Column('pubkey', Text),
@@ -25,7 +23,7 @@ posts = Table('frontend_posts', _meta,
               Column('comment', Text))
 
 files = Table('frontend_files', _meta,
-              Column('file_id', GUID, primary_key=True),
+              Column('file_id', Integer, primary_key=True),
               Column('filename', Text, nullable=False),
               Column('filepath', Text, nullable=False),
               Column('parent', Text, ForeignKey('frontend_posts.article_id')))

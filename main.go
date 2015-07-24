e98db023309786f9f246a7ebed4737c3ee216c06
jeff <ampernand@gmail.com>
2015-07-24 12:08:46 -0400

    change arguments

--- main.go
+++ main.go
@@ -1,31 +1,32 @@
 package main
 
 import (
-  "flag"
+  "fmt"
   "github.com/majestrate/srndv2/src/srnd"
   "log"
+  "os"
 )
-
-var action string
-
-func init() {
-  flag.StringVar(&action, "action", "", "what action will we run? (setup, run)")
-}
 
 
 func main() {
   var daemon srnd.NNTPDaemon
-  flag.Parse()
-  if action == "setup" {
-    log.Println("Setting up SRNd base...")
-    daemon.Setup()
-    log.Println("Setup Done")
-  } else if action == "run" {
-    log.Println("Starting up SRNd...")
-    if daemon.Init() {
-      daemon.Run()
+  if len(os.Args) > 1 {
+    action := os.Args[1]
+    if action == "setup" {
+      log.Println("Setting up SRNd base...")
+      daemon.Setup()
+      log.Println("Setup Done")
+    } else if action == "run" {
+      log.Println("Starting up SRNd...")
+      if daemon.Init() {
+        daemon.Run()
+      } else {
+        log.Println("Failed to initialize")
+      }
     } else {
-      log.Println("Failed to initialize")
+      log.Println("Invalid action:",action)
     }
+  } else {
+    fmt.Fprintf(os.Stdout, "Usage: %s [setup|run]\n", os.Args[0])
   }
 }

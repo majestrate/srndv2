d370b00ba857f452377a2a0d88ca6dadb0ec8b57
jeff <ampernand@gmail.com>
2015-08-03 13:22:31 -0400

    when relaying always base64 encode

--- src/srnd/message.go
+++ src/srnd/message.go
@@ -254,10 +254,10 @@ func (self nntpArticle) WriteBody(wr io.Writer) (err error) {
         if err != nil {
           log.Println("error writing part", err)
         }
+        hdr.Set("Content-Transfer-Encoding", "base64")
         if strings.HasPrefix(att.Mime(), "text/plain") {
           err = att.WriteTo(part)
         } else {
-          hdr.Set("Content-Transfer-Encoding", "base64")
           enc := base64.NewEncoder(base64.StdEncoding, part)
           err = att.WriteTo(enc)
           enc.Close()  

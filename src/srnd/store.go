de585febd37b8a7614634393fc41a36b534aac37
jeff <ampernand@gmail.com>
2015-08-07 18:01:00 -0400

    log sha512 for debugging

--- src/srnd/store.go
+++ src/srnd/store.go
@@ -13,6 +13,7 @@ import (
   "crypto/sha512"
   "encoding/base32"
   "encoding/base64"
+  "encoding/hex"
   "errors"
   "fmt"
   "io"
@@ -225,6 +226,7 @@ func (self articleStore) ReadMessage(r io.Reader) (NNTPMessage, error) {
         pk_bytes := unhex(pk)
         sig_bytes := unhex(sig)
         body_bytes := sha512.Sum512(smsg.Bytes())
+        log.Printf("body sum=%s", hex.EncodeToString(body_bytes[:]))
         if nacl.CryptoVerifyFucky(body_bytes[:], sig_bytes, pk_bytes) {
           log.Println("signature is valid :^)")
         } else {

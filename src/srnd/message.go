c3b6a517636b9c8d3fef9cdfc0e4b97923a72413
jeff <ampernand@gmail.com>
2015-08-07 22:33:15 -0400

    add temp logging

--- src/srnd/message.go
+++ src/srnd/message.go
@@ -246,6 +246,7 @@ func (self nntpArticle) WriteBody(wr io.Writer) (err error) {
     for {
       // convert line endings :\
       line, err = r.ReadBytes('\n')
+      log.Println(line)
       if err == nil {
         w.Write(line[:len(line)-2])
         w.WriteByte(10)

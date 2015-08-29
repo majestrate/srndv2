76869d02d2bbabc4b2bbfe656b4417279d6af377
jeff <ampernand@gmail.com>
2015-08-29 11:03:37 -0400

    mehhhh

--- src/srnd/daemon.go
+++ src/srnd/daemon.go
@@ -264,7 +264,7 @@ func (self *NNTPDaemon) polloutfeeds() {
         if use && feed.policy != nil {
           if feed.policy.AllowsNewsgroup(nntp.Newsgroup()) {
             if feed.info.mode == "reader" {
-              log.Println("ask for", nntp.MesaageID())
+              log.Println("ask for", nntp.MessageID())
               feed.sync <- nntp
             }
           }

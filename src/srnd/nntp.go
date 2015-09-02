bfd3ff0d7ca87ce1f4c1daae7e2b6f2057935370
jeff <ampernand@gmail.com>
2015-09-02 14:24:13 -0400

    && - > ||

--- src/srnd/nntp.go
+++ src/srnd/nntp.go
@@ -288,7 +288,7 @@ func (self nntpConnection) handleLine(daemon NNTPDaemon, code int, line string, 
             // invalid newsgroup format
             reason = "invalid newsgroup"
             code = 439
-          } else if ! ( ValidMessageID(msgid) && ( reference != "" && ! ValidMessageID(reference) ) ) {
+          } else if ! ( ValidMessageID(msgid) || ( reference != "" && ! ValidMessageID(reference) ) ) {
             // invalid message id or reference
             reason = "invalid reference or message id is '" + msgid + "' reference is '"+reference + "'"
             code = 439

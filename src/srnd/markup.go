fe4d89e10871dbcc553cbd7d2d3871ed4881d750
jeff <ampernand@gmail.com>
2015-09-04 18:17:25 -0400

    sorround in quotes

--- src/srnd/markup.go
+++ src/srnd/markup.go
@@ -12,27 +12,29 @@ import (
 
 
 // copypasted from https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
-var re_external_link = regexp.MustCompile(`(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?`);
+var re_external_link = regexp.MustCompile(`((?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)`);
 
 
 func formatline(line string) (markup string) {
   line = strings.Trim(line, "\t\r\n ")
-  if strings.HasPrefix(line, ">") {
-    // le ebin meme arrows
-    markup += "<p><span class='memearrows'>"
-    markup += html.EscapeString(line)
-    markup += "</span></p>"
-  } else if strings.HasPrefix(line, "==") && strings.HasSuffix(line, "==") {
-    // redtext
-    markup += "<p><span class='redtext'>"
-    markup += html.EscapeString(line[2:len(line)-2])
-    markup += "</span></p>"
-  } else {
-    // regular line
-    markup += "<p>"
-    // linkify it
-    markup += re_external_link.ReplaceAllString(line, `<a href="$1">$1</a>`)
-    markup += "</p>"
+  if len(line) > 0 {
+    if strings.HasPrefix(line, ">") {
+      // le ebin meme arrows
+      markup += "<p><span class='memearrows'>"
+      markup += html.EscapeString(line)
+      markup += "</span></p>"
+    } else if strings.HasPrefix(line, "==") && strings.HasSuffix(line, "==") {
+      // redtext
+      markup += "<p><span class='redtext'>"
+      markup += html.EscapeString(line[2:len(line)-2])
+      markup += "</span></p>"
+    } else {
+      // regular line
+      markup += "<p>"
+      // linkify it
+      markup += re_external_link.ReplaceAllString(line, `<a href="$1">$1</a>`)
+      markup += "</p>"
+    }
   }
   return
 }

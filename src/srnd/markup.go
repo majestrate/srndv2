99bb282352bc8252de80f40012d1273457ff063d
jeff <ampernand@gmail.com>
2015-10-14 08:03:44 -0400

    bump the short hash size to 18

--- src/srnd/markup.go
+++ src/srnd/markup.go
@@ -13,7 +13,7 @@ import (
 
 // copypasted from https://stackoverflow.com/questions/161738/what-is-the-best-regular-expression-to-check-if-a-string-is-a-valid-url
 // var re_external_link = regexp.MustCompile(`((?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?)`);
-var re_backlink = regexp.MustCompile(`>> ?([0-9a-f]{10})`)
+var re_backlink = regexp.MustCompile(`>> ?([0-9a-f]+)`)
 
 // parse backlink
 func backlink(word string) (markup string) {

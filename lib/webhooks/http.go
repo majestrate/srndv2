20ef8b70e3a4d1fb38e378834c668f3934579d93
Jeff Becker <ampernand@gmail.com>
2016-08-11 11:08:46 -0400

    try doing part rewrite in webhooks

--- lib/webhooks/http.go
+++ lib/webhooks/http.go
@@ -9,6 +9,8 @@ import (
 
 	"io"
 	"io/ioutil"
+	"mime"
+	"mime/multipart"
 	"net/http"
 	"net/textproto"
 	"net/url"
@@ -48,8 +50,56 @@ func (h *httpWebhook) GotArticle(msgid nntp.MessageID, group nntp.Newsgroup) {
 			}
 			q.Set("Content-Type", ctype)
 			u.RawQuery = q.Encode()
+
+			var body io.Reader
+
+			if strings.HasPrefix(ctype, "multipart") {
+				pr, pw := io.Pipe()
+				body = pr
+				go func(in io.Reader, out io.WriteCloser) {
+					_, params, _ := mime.ParseMediaType(ctype)
+					if params == nil {
+						// send as whatever lol
+						io.Copy(out, in)
+					} else {
+						boundary, _ := params["boundary"]
+						mpr := multipart.NewReader(in, boundary)
+						mpw := multipart.NewWriter(out)
+						mpw.SetBoundary(boundary)
+						for {
+							part, err := mpr.NextPart()
+							if err == io.EOF {
+								err = nil
+								break
+							} else if err == nil {
+								// get part header
+								h := part.Header
+								// rewrite header part for php
+								cd := h.Get("Content-Disposition")
+								t, p, _ := mime.ParseMediaType(cd)
+								p["name"] = "attachment[]"
+								h.Set("Content-Disposition", mime.FormatMediaType(t, p))
+								// make write part
+								wp, err := mpw.CreatePart(h)
+								if err == nil {
+									// write part out
+									io.Copy(wp, part)
+								} else {
+									log.Errorf("error writng webhook part: %s", err.Error())
+								}
+							}
+							part.Close()
+						}
+						mpw.Close()
+					}
+					out.Close()
+				}(c.R, pw)
+			} else {
+				body = c.R
+			}
+
 			var r *http.Response
-			r, err = http.Post(u.String(), ctype, c.R)
+			r, err = http.Post(u.String(), ctype, body)
 			if err == nil {
 				_, err = io.Copy(ioutil.Discard, r.Body)
 				r.Body.Close()
